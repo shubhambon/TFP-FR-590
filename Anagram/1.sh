701866d318426ea41866fcdb23764d803dfd4450
shubham <shubham.bondre.1995@gmail.com>
2022-09-30 18:21:38 +0530

    [shubham]first commit

--- Anagram/1.sh
+++
@@ -1,5 +0,0 @@
-if echo $var | egrep -q '^[0-9]+$'; then
-    # $var is a number
-else
-    # $var is not a number
-fi
---
+++ Java/FirstJavaProgrameCWH/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("The sum of these number");
+        int num1 = 6;
+        int num2 = 5;
+        int num3 = 9;
+        int num4 = 2;
+        int sum  = num1 + num2 + num3 + num4;
+        System.out.println(sum);
+
+    }
+
+}
